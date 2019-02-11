a5222d85b7cba820e464c175d2d5f37ff3b222ff
Haile, Aberham <hailea3@nationwide.com>
2019-02-11 15:25:38 -0500

    adada

--- web/controllers/task_controller.ex
+++ web/controllers/task_controller.ex
@@ -6,8 +6,10 @@ defmodule PhoenixTasks.TaskController do
   alias PhoenixTasks.User
 
   def index(conn, params, user) do
-    tasks = Repo.all(Task)
-    render(conn, "index.html", tasks: tasks)
+  user =
+    user |>
+      Repo.preload(:tasks)
+    render(conn, "index.html", company: params["customer_id"], project: params["project_id"], tasks: user.tasks)
   end
 
   def new(conn, params, user) do
@@ -16,18 +18,20 @@ defmodule PhoenixTasks.TaskController do
   end
 
   def create(conn, %{"task" => task_params}, user) do
-    require IEx; IEx.pry()
-    project = Repo.get(Project, conn.params["project_id"])
+    customer = conn.params["customer_id"]
     changeset = Repo.get(Project, conn.params["project_id"])
                 |> build_assoc(:tasks)
-                |> Task.changeset(task_params)
-    changeset = Task.changeset(%Task{}, task_params)
+                |> Ecto.Changeset.change()
+                |> Ecto.Changeset.put_assoc(:users, user)
+
+    changeset = Task.changeset(changeset, task_params)
     case Repo.insert(changeset) do
       {:ok, task} ->
         conn
         |> put_flash(:info, "Task created successfully.")
+        |> redirect(to: customer_project_task_path(conn, :show, customer, task.project_id, task.user_id))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+#       gotta fix this render(conn, "new.html", changeset: changeset)
     end
   end
 
@@ -39,30 +43,33 @@ defmodule PhoenixTasks.TaskController do
   def edit(conn, %{"id" => id}, user) do
     task = Repo.get(Task, id)
     changeset = Task.changeset(task)
-    render(conn, "edit.html", task: task, changeset: changeset)
+    render(conn, "edit.html", task: task, changeset: changeset, customer: conn.params["customer_id"], project: conn.params["project_id"])
   end
 
   def update(conn, %{"id" => id, "task" => task_params}, user) do
+#    require IEx; IEx.pry()
     task = Repo.get(Task, id)
     changeset = Task.changeset(task, task_params)
-
+    customer = conn.params["customer_id"]
+    project = conn.params["project_id"]
     case Repo.update(changeset) do
       {:ok, task} ->
         conn
         |> put_flash(:info, "Task updated successfully.")
-#        |> redirect(to: customer_project_task_path(conn, :show, task))
+        |> redirect(to: customer_project_task_path(conn, :show, customer, project, task.id))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", task: task, changeset: changeset)
     end
   end
 
   def delete(conn, %{"id" => id}, user) do
+    customer = conn.params["customer_id"]
+    project = conn.params["customer_id"]
     task = Repo.get(Task, id)
-    {:ok, _task} = PhoenixTasks.delete_task(task)
-
+    Repo.delete(task)
     conn
-    |> put_flash(:info, "Task deleted successfully.")
-#    |> redirect(to: customer_project_task_path(conn, :index))
+      |> put_flash(:info, "Task deleted successfully.")
+      |> redirect(to: customer_project_task_path(conn, :index, customer, project, 1))
   end
 
     def action(conn, _) do
